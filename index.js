5a5f27095d96a1b3f2a34333c91f14353cdbd4e0
Fabian Dill <fabiandill@produktmacher.com>
2016-07-23 20:52:55 +0200

    added some documentation

--- index.js
+++ index.js
@@ -3,26 +3,31 @@
 const config = require('./config');
 const bodyParser = require('body-parser');
 const express = require('express');
-var request = require('request');
+const request = require('request');
 
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
     throw new Error('missing FB_PAGE_TOKEN');
 }
 
-var token = config.FB_PAGE_TOKEN
+/* The Facebook access token. */
+var token = config.FB_PAGE_TOKEN;
 
-/*
+
+/* This won't work with Amazon Lambda! And even for Heroku the sessions are
+  only stored a certain amount of time. In any case>: make sure to persist the
+  sessions and user settings in a data base. */
 const sessions = {};
-const findOrCreateSession = (sessions, fbid, cb) => {
+var nr_sessions = 0;
 
-    if (!sessions[fbid]) {
-        console.log("New Session for:", fbid);
-        sessions[fbid] = session;
-        cb(sessions, fbid);
-    }
-};
-*/
+function findOrCreateSession(sender_id) {
+	if(typeof sessions[sender_id] == "undefined") {
+		sessions[sender_id] = {};
+		nr_sessions++;
+		console.log("created new session for " + sender_id + ". Total number of sessions: " + nr_sessions);
+	}
+	return sessions[sender_id];
+}
 
 // Starting our webserver and putting it all together
 // app configuration according to this tutorial:
@@ -33,7 +38,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.get('/', function(req, res) {
 	res.send('Hello world! I am a chatbot demo')
-}) 
+})
 app.get('/webhook', function(req, res) {
 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
@@ -41,17 +46,27 @@ app.get('/webhook', function(req, res) {
 		res.send('Error, wrong token')
 	}
 })
-// spin up server 
+// spin up server
 app.listen(app.get('port'), function() {
 	console.log('running on port ' + app.get('port'))
 })
 
 // Message handler
 app.post('/webhook', function(req, res) {
+  /*
+  entry[0] is the first facebook page. If your bot listens to several
+  pages zou have to iterate over the entries as well. */
 	var messaging_events = req.body.entry[0].messaging
 	for (var i = 0; i < messaging_events.length; i++) {
 		var event = req.body.entry[0].messaging[i]
 		var sender = event.sender.id
+    findOrCreateSession(sender);
+    // handle quick replies before text messages, since they also contain text
+    if(event.message && event.message.quick_reply) {
+      handleQuickReply(sender, event.message)
+      continue;
+    }
+    // check whether we have a text message here
 		if (event.message && event.message.text) {
 			var text = event.message.text
 			handleTextMessage(sender, text)
@@ -87,7 +102,13 @@ function handleTextMessage(sender, message_text) {
 function handlePostback(sender, postback_text) {
 	console.log('postback: ' + postback_text)
 	/* TODO: process your postback codes here. Either if else or switch. */
-	sendTextMessage(sender, 'Postback received: ' + postback_text) 
+	sendTextMessage(sender, 'Postback received: ' + postback_text)
+}
+
+function handleQuickReply(sender, message) {
+  console.log('handleQuickReply: ' + JSON.stringify(message));
+  var payload = message.quick_reply.payload
+  // TODO: handle quick reply here
 }
 
 // generic text message
@@ -122,7 +143,7 @@ function processAnswer(sender, textmessage) {
 	var intent = 'None'
 	var entity = ''
 	request({
-		url: 'https://api.projectoxford.ai/luis/v1/application?id=' + config.LUIS_APP_ID 
+		url: 'https://api.projectoxford.ai/luis/v1/application?id=' + config.LUIS_APP_ID
 				+ '&subscription-key=' + config.LUIS_SUBSCRIPTION_KEY + '&q=' + textmessage,
 	}, function(error, response, body) {
 			if(error) {
